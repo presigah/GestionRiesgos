98ee0939528defcf877d385da308209c11a31807
Aura Russill <auracris1996@hotmail.com>
2022-06-25 00:18:05 -0500

    ADD:Entidad y dto project
    se crea la entidad de porject y el dto con sus respectivos atributos

---
+++ src/main/java/co/com/sofka/gestionriesgos/mappers/ProjectMapper.java
@@ -0,0 +1,4 @@
+package co.com.sofka.gestionriesgos.mappers;
+
+public class ProjectMapper {
+}
--- src/main/java/co/com/sofka/gestionriesgos/model/ProjectDTO.java
+++ src/main/java/co/com/sofka/gestionriesgos/model/ProjectDTO.java
@@ -1,4 +1,22 @@
 package co.com.sofka.gestionriesgos.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProjectDTO {
+
+    private Integer id;
+    private String name;
+    private LocalDate startDate;
+    private LocalDate endingDate;
+    private List<String> labels;
+    private List<String> emails;
+    private String description;
 }
