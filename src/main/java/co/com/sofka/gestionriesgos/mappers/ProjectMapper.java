d7ea430121a105b38054646f262fcf194d795f7e
Aura Russill <auracris1996@hotmail.com>
2022-06-25 08:51:09 -0500

    ADD:Configuracion-inicial-Back-Mapper
    Creacion de ProjectMapper

--- src/main/java/co/com/sofka/gestionriesgos/mappers/ProjectMapper.java
+++ src/main/java/co/com/sofka/gestionriesgos/mappers/ProjectMapper.java
@@ -1,4 +1,34 @@
 package co.com.sofka.gestionriesgos.mappers;
 
+import co.com.sofka.gestionriesgos.collections.Project;
+import co.com.sofka.gestionriesgos.model.ProjectDTO;
+
+import java.util.function.Function;
+
 public class ProjectMapper {
+
+    public Function<ProjectDTO, Project> toProject(){
+        return newProject -> {
+            var project = new Project();
+            project.setName(newProject.getName());
+            project.setStartDate(newProject.getStartDate());
+            project.setEndingDate(newProject.getEndingDate());
+            project.setLabels(newProject.getLabels());
+            project.setEmails(newProject.getEmails());
+            project.setDescription(newProject.getDescription());
+            return project;
+        };
+    }
+
+    public Function<Project, ProjectDTO> toProjectDTO(){
+        return entity -> new ProjectDTO(
+                entity.getId(),
+                entity.getName(),
+                entity.getStartDate(),
+                entity.getEndingDate(),
+                entity.getLabels(),
+                entity.getEmails(),
+                entity.getDescription()
+        );
+    }
 }
--- src/main/java/co/com/sofka/gestionriesgos/repositories/ProjectRepository.java
+++ src/main/java/co/com/sofka/gestionriesgos/repositories/ProjectRepository.java
@@ -1,4 +1,7 @@
 package co.com.sofka.gestionriesgos.repositories;
 
+import co.com.sofka.gestionriesgos.collections.Project;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+
 public interface ProjectRepository {
 }
