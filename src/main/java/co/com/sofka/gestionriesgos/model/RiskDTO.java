441fa00cb181cd98b76c85a39d2af3f77ed3ffcc
Jorge Reyes <reyesjorge0116@gmail.com>
2022-06-25 13:16:58 -0500

    REF: RIESGO-BACK-CONFIGURACION INICIAL
    Se refactorizan los campos de la entidad y el dto Risk con su mapper

--- src/main/java/co/com/sofka/gestionriesgos/model/RiskDTO.java
+++ src/main/java/co/com/sofka/gestionriesgos/model/RiskDTO.java
@@ -1,4 +1,81 @@
 package co.com.sofka.gestionriesgos.model;
 
+import javax.validation.constraints.NotBlank;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class RiskDTO {
+
+    private String id;
+
+    @NotBlank
+    private String projectId;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String userId;
+
+    @NotBlank
+    private LocalDate detectedDate = LocalDate.now();
+
+    @NotBlank
+    private LocalDate endedDate;
+
+    private List<String> labels;
+
+    @NotBlank
+    private String description;
+
+    @NotBlank
+    // (Abierto; mitigado; Cerrado; Problema)
+    private String risksState;
+
+    @NotBlank
+    // (Interna; Externa)
+    private String audience;
+
+    @NotBlank
+    // (Costo; Tiempo; Calidad)
+    private String category;
+
+    @NotBlank
+    // (Riesgo de producto o calidad; Riesgo de proyecto)
+    private String riskType;
+
+    @NotBlank
+    private String detailsRiskType;
+
+    @NotBlank
+    private Integer probability;
+
+    @NotBlank
+    private Integer impactValue;
+
+    @NotBlank
+    private String mitigationPlan;
+
+    private List<String> responsibleMitigationMails;
+
+    @NotBlank
+    private String contingencyPlan;
+
+    private List<String> responsibleContingencyMails;
+
+    // ------
+    private String projectName;
+
 }
--- src/main/java/co/com/sofka/gestionriesgos/repositories/RiskRepository.java
+++ src/main/java/co/com/sofka/gestionriesgos/repositories/RiskRepository.java
@@ -1,4 +1,10 @@
 package co.com.sofka.gestionriesgos.repositories;
 
-public interface RiskRepository {
+import co.com.sofka.gestionriesgos.collections.Risk;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
+
+public interface RiskRepository extends ReactiveCrudRepository<Risk, Integer> {
+    Flux<Risk> findByUserId();
+    Flux<Risk> findByProjectId();
 }
