0c427bf6e970a798ba11d259c6fe5657def5c29b
pilar <ingeniera.coral@gmail.com>
2022-06-26 00:11:21 -0500

    agregando ruta  del componenete risk-table

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RiskDetailComponent } from './risk-detail/risk-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'risk', component: RiskDetailComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
