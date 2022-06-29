eb695e1a221f7925ed15a14f337ac85c6821f807
Maricela del Pilar Coral <86802151+Pilar89@users.noreply.github.com>
2022-06-29 11:05:47 -0500

    Maricela (#24)
    * completando funcionalidad de crear riesgo
    * agregando componenete que permite editar riesgo
    * agregando funcionalidad editar riesgo
    * agregando funcionalidad de eliminar riesgo

--- web/src/app/risk-table/risk-table.component.ts
+++ web/src/app/risk-table/risk-table.component.ts
@@ -1,7 +1,21 @@
-import { Component, Input, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { RiskService } from './../service/risk.service';
+import {
+  Component,
+  Input,
+  OnInit,
+  QueryList,
+  ViewChildren,
+} from '@angular/core';
 import { Risk } from '../models/risk';
-import { SortableHeaderRiskDirective, SortRiskEvent } from '../directives/sortable-header-risk.directive';
-import { faHeartCirclePlus, faArrowUpRightFromSquare, faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import {
+  SortableHeaderRiskDirective,
+  SortRiskEvent,
+} from '../directives/sortable-header-risk.directive';
+import {
+  faHeartCirclePlus,
+  faArrowUpRightFromSquare,
+  faTrashCan,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-risk-table',
@@ -9,9 +23,14 @@ import { faHeartCirclePlus, faArrowUpRightFromSquare, faTrashCan } from '@fortaw
   styleUrls: ['./risk-table.component.css'],
 })
 export class RiskTableComponent implements OnInit {
-  @ViewChildren(SortableHeaderRiskDirective) headers: QueryList<SortableHeaderRiskDirective> | undefined;
+  @ViewChildren(SortableHeaderRiskDirective) headers:
+    | QueryList<SortableHeaderRiskDirective>
+    | undefined;
 
-  compare = (v1: string | Date | [string] | number, v2: string | Date | [string] | number ) => v1 < v2 ? -1 : v1 > v2 ? 1 : 0;
+  compare = (
+    v1: string | Date | string[] | number,
+    v2: string | Date | string[] | number
+  ) => (v1 < v2 ? -1 : v1 > v2 ? 1 : 0);
 
   faHeartCirclePlus = faHeartCirclePlus;
   faArrowUpRightFromSquare = faArrowUpRightFromSquare;
@@ -20,35 +39,40 @@ export class RiskTableComponent implements OnInit {
   page: number = 1;
 
   @Input() risks: Risk[] = [];
-  constructor() {}
+  constructor(private service: RiskService) {}
 
   ngOnInit(): void {}
 
-  onRiskSort({columnRisk, directionRisk}: SortRiskEvent) {
+  onRiskSort({ columnRisk, directionRisk }: SortRiskEvent) {
     // resetting other headers
-    this.headers?.forEach(header => {
+    this.headers?.forEach((header) => {
       if (header.sortableRisk !== columnRisk) {
         header.directionRisk = '';
       }
     });
 
-    if(directionRisk === '' || columnRisk == null){
+    if (directionRisk === '' || columnRisk == null) {
       this.risks;
     } else {
       this.risks.sort((a, b) => {
-        if (columnRisk == null || columnRisk == '') return 0
-        const aColumnRisk = a[columnRisk]
-        const bColumnRisk = b[columnRisk]
-        if (aColumnRisk == null && bColumnRisk == null) return 0
-        if (aColumnRisk == null) return 1
-        if (bColumnRisk == null) return -1
+        if (columnRisk == null || columnRisk == '') return 0;
+        const aColumnRisk = a[columnRisk];
+        const bColumnRisk = b[columnRisk];
+        if (aColumnRisk == null && bColumnRisk == null) return 0;
+        if (aColumnRisk == null) return 1;
+        if (bColumnRisk == null) return -1;
         const res = this.compare(aColumnRisk, bColumnRisk);
         return directionRisk === 'asc' ? res : -res;
       });
     }
   }
-    
+
   deleteRisk(risk: Risk) {
-    console.log(risk);
+    risk.state = 0;
+    this.service.updateRisk(risk).subscribe(() => {
+      setTimeout(() => {
+        window.location.reload();
+      }, 1000);
+    });
   }
 }
