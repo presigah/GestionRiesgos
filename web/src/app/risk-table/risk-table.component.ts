82c7688e7c3fa921281fb8e09a0d3970149f3d96
pilar <ingeniera.coral@gmail.com>
2022-06-28 21:56:34 -0500

    completando funcionalidad de crear riesgo

--- web/src/app/risk-table/risk-table.component.ts
+++ web/src/app/risk-table/risk-table.component.ts
@@ -1,6 +1,15 @@
-import { Component, Input, OnInit, QueryList, ViewChildren } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  QueryList,
+  ViewChildren,
+} from '@angular/core';
 import { Risk } from '../models/risk';
-import { SortableHeaderRiskDirective, SortRiskEvent } from '../directives/sortable-header-risk.directive';
+import {
+  SortableHeaderRiskDirective,
+  SortRiskEvent,
+} from '../directives/sortable-header-risk.directive';
 import { faHeartCirclePlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -9,9 +18,14 @@ import { faHeartCirclePlus } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./risk-table.component.css'],
 })
 export class RiskTableComponent implements OnInit {
-  @ViewChildren(SortableHeaderRiskDirective) headers: QueryList<SortableHeaderRiskDirective> | undefined;
+  @ViewChildren(SortableHeaderRiskDirective) headers:
+    | QueryList<SortableHeaderRiskDirective>
+    | undefined;
 
-  compare = (v1: string | Date | [string] | number, v2: string | Date | [string] | number ) => v1 < v2 ? -1 : v1 > v2 ? 1 : 0;
+  compare = (
+    v1: string | Date | string[] | number,
+    v2: string | Date | string[] | number
+  ) => (v1 < v2 ? -1 : v1 > v2 ? 1 : 0);
 
   faHeartCirclePlus = faHeartCirclePlus;
 
@@ -22,30 +36,30 @@ export class RiskTableComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  onRiskSort({columnRisk, directionRisk}: SortRiskEvent) {
+  onRiskSort({ columnRisk, directionRisk }: SortRiskEvent) {
     // resetting other headers
-    this.headers?.forEach(header => {
+    this.headers?.forEach((header) => {
       if (header.sortableRisk !== columnRisk) {
         header.directionRisk = '';
       }
     });
 
-    if(directionRisk === '' || columnRisk == null){
+    if (directionRisk === '' || columnRisk == null) {
       this.risks;
     } else {
       this.risks.sort((a, b) => {
-        if (columnRisk == null || columnRisk == '') return 0
-        const aColumnRisk = a[columnRisk]
-        const bColumnRisk = b[columnRisk]
-        if (aColumnRisk == null && bColumnRisk == null) return 0
-        if (aColumnRisk == null) return 1
-        if (bColumnRisk == null) return -1
+        if (columnRisk == null || columnRisk == '') return 0;
+        const aColumnRisk = a[columnRisk];
+        const bColumnRisk = b[columnRisk];
+        if (aColumnRisk == null && bColumnRisk == null) return 0;
+        if (aColumnRisk == null) return 1;
+        if (bColumnRisk == null) return -1;
         const res = this.compare(aColumnRisk, bColumnRisk);
         return directionRisk === 'asc' ? res : -res;
       });
     }
   }
-    
+
   deleteRisk(risk: Risk) {
     console.log(risk);
   }
